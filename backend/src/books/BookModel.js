500d3553e0dda09b77464273fea74ac7570c1017
Oicali <121882893+Oicali@users.noreply.github.com>
2025-10-05 07:37:06 +0800

    Created Database
    Created DB, and books folder (Routing, Controller, Model)

--- backend/src/books/BookModel.js
+++ backend/src/books/BookModel.js
@@ -40,6 +40,18 @@ const bookSchema = new mongoose.Schema(
   }
 );
 
+
+// const moment = require("moment-timezone");
+
+// // Virtual field to display PH time
+// bookSchema.virtual("createdAtPH").get(function () {
+//   return moment(this.createdAt).tz("Asia/Manila").format("YYYY-MM-DD HH:mm:ss");
+// });
+
+// // Ensure virtuals are included when converting to JSON
+// bookSchema.set("toJSON", { virtuals: true });
+// bookSchema.set("toObject", { virtuals: true });
+
 const Book = mongoose.model("Book", bookSchema);
 
 module.exports = Book;
--- backend/src/books/BookRoute.js
+++ backend/src/books/BookRoute.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const Book = require('./BookModel');
-// const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./BookController');
+const { postABook, getAllBooks, getSingleBook, updateBook, deleteABook } = require('./BookController');
 //      const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router =  express.Router();
 
-// post a book
+// create a book endpoint
+router.post("/create-book", postABook);
 //      router.post("/create-book", verifyAdminToken, postABook)
 
-// get all books
-//router.get("/", getAllBooks);
+// all books endpoint
+router.get("/", getAllBooks);
 
 // single book endpoint
-//router.get("/:id", getSingleBook);
+router.get("/:id", getSingleBook);
 
 // update a book endpoint
+router.put("/edit-book/:id", updateBook);
 //      router.put("/edit/:id", verifyAdminToken, UpdateBook);
 
+// delete a book endpoint
+router.delete("/:id", deleteABook);
+
 //      router.delete("/:id", verifyAdminToken, deleteABook)
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
